d8c5cfa30d68f52fe5686ae6ec02adac378a3cd4
Matthew Hall <Matthew Hall@DESKTOP-9TEFHER.wifi.uc.edu>
2022-10-25 09:49:42 -0400

    Start of Assignment09

---
+++ Assignment09/MainPackage/main.py
@@ -0,0 +1,17 @@
+'''
+Name: Group Balin
+email: hall4mj@ucmail.uc.edu ADD YOUR EMAILS HERE!
+Assignment: Assignment 09
+Course: IS 4010
+Semester/Year: Fall 2022
+Brief Description: This project demonstrates that we can collaborate on a project using github and know how to use Object Oriented programming.
+Citations:
+Anything else that's relevant:
+'''
+from ClassPackage.MammalClass import Mammal
+
+if __name__ == "__main__":
+# Instaniates the myAnimal object
+    myAnimal = Mammal("Bear", "Fish", "Forest")
+    # here is the __str___ requirement
+    print (myAnimal.__str__())
