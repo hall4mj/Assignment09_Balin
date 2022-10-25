1b077d7c168333b0ba0f0fc3d13f15f1fbe54716
Sarah Zimmer <Sarah Zimmer@DESKTOP-Q3NRSDA.lan>
2022-10-25 12:36:48 -0400

    Created Inventory.py, added formatting to main.py

---
+++ Assignment09/InventoryPackage/Inventory.py
@@ -0,0 +1,59 @@
+'''
+Sarah Zimmer
+Purpose: Model inventory at the Cincinnati Bengals merchandise store
+'''
+class Jerseys:
+# Getter / Setters
+    # name
+    def get_name(self):
+        return self.get_name 
+    def set_name(self, name):
+        self.get_name(name)
+    # sleeve length
+    def get_sleeves(self, sleeves):
+        if sleeves == "Short" or "Long":
+            print("Sleeve length must be either Short or Long.")
+        else:
+            return self.get_sleeves
+    def set_sleeves(self, sleeves):
+        self.get_sleeves(sleeves)
+    # jersey color
+    def get_color(self, color):
+        if color == "Black" or "White" or "Orange" or "Pink":
+            print("Not a valid jersey color.")
+        else:
+            return self.get_color
+    def set_color(self, color):
+        self.get_color(color)
+    # quantity
+    def get_quantity(self, quantity):
+        if quantity < 0:
+            print("Jersey quantity cannot be less than zero")
+        else:
+            self.get_quantity(quantity)
+    def set_quantity(self, quantity):
+        self.get_quantity(quantity)
+# init 
+    def __init__(self, name, sleeves, color, quantity):
+        self.name = name;
+        self.sleeves = sleeves;
+        self.color = color;
+        self.quantity = quantity;
+        
+# Dunder methods
+    def __str__(self):
+        return "Name= " + self.name + ", Sleeve Length= " + str(self.sleeves) + ", Color= " + str(self.color) + ", Stock quantity= " + str(self.quantity)
+    def __repr__(self):
+        return "Name: " + self.name + ", Sleeve Length: " + self.sleeves + ", Color: " + self.color + ", Stock quantity: " + str(self.quantity)
+    
+# Method 1: Check if an item is needed to be restocked
+    def restock(self):
+        if self.quantity <= 10:
+            return "Additional stock of " + self.name + " needs to be ordered."
+        else:
+            return "Acceptable quantity in inventory."
+    
+# Method 2: Change the value of quantity for an instance
+    def changeQuantity(self,x):
+        self.quantity = x
+        return "The updated quantity of " + self.name + " is " + str(self.quantity) + "."
--- Assignment09/MainPackage/main.py
+++ Assignment09/MainPackage/main.py
@@ -1,6 +1,6 @@
 '''
 Name: Group Balin
-email: hall4mj@ucmail.uc.edu ADD YOUR EMAILS HERE!
+email: hall4mj@ucmail.uc.edu , zimmese@mail.uc.edu
 Assignment: Assignment 09
 Course: IS 4010
 Semester/Year: Fall 2022
@@ -8,6 +8,7 @@ Brief Description: This project demonstrates that we can collaborate on a projec
 Citations:
 Anything else that's relevant:
 '''
+print("Mammal Module Results:")
 from ClassPackage.MammalClass import Mammal
 
 if __name__ == "__main__":
@@ -15,3 +16,19 @@ if __name__ == "__main__":
     myAnimal = Mammal("Bear", "Fish", "Forest")
     # here is the __str___ requirement
     print (myAnimal.__str__())
+
+### Inventory Module ###
+print("Inventory Module Results:")
+from InventoryPackage.Inventory import *
+
+#Instances of class
+JB_01 = Jerseys("Joe Burrow Jersey", "Short", "Black", 16)
+JC_01 = Jerseys("Ja'Marr Chase Jersey", "Long", "White", 10)
+
+# Dunder Results
+print(JB_01.__repr__())
+print(JC_01.__str__())
+
+# Method Results
+print(JC_01.restock())
+print(JB_01.changeQuantity(30))
